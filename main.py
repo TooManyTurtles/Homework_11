dc758475756ef061ca2fcf06eba2c5baac94724c
Tony Goodman <syedinanton@gmail.com>
2023-08-07 21:50:29 +0300

    Is it better?

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
-# This is a sample Python script.
-#
+
 # 1. Даний текстовий файл. Необхідно створити новий файл, який потрібно переписати з першого файлу всі слова,
 # що складаються не менше ніж з семи літер.
 # 2. Даний текстовий файл. Підрахувати кількість слів у ньому.
-# Додатково:
-# Створіть програму, яка перевіряє текст на неприпустимі слова.
-# Якщо неприпустиме слово знайдено, його слід замінити на набір символів *.
-# За підсумками роботи програми необхідно показати статистику дій.
-# Наприклад, якщо й у нас є такий текст:
+
 # To be, or not to be, that is the question, Whether 'tis nobler in the mind to suffer The slings and arrows of
 # outrageous fortune, Or to take arms against a sea of troubles, And by opposing end them? To die: to sleep;
 # No more; and by a sleep to say we end The heart-ache and the thousand natural shocks That flesh is heir to,
 # 'tis a consummation Devoutly to be wish'd. To die, to sleep.
-# Неприпустиме слово: die.
-# Результат:
-# To be, or not to be, that is the question, Whether 'tis nobler in the mind to suffer The slings and arrows of
-# outrageous fortune, Or to take arms against a sea of troubles, And by opposing end them? To ***: to sleep;
-# No more; and by a sleep to say we end The heart-ache and the thousand natural shocks That flesh is heir to,
-# 'tis a consummation Devoutly to be wish'd. To ***, to sleep.
-# Статистика: 2 заміни слова die.
-# Нотатка:
-# Текст для всіх завдань можна написати самостійно або взяти з Інтернету.
-# Логіка має працювати з будь-яким текстом.
+
 
 
 with open("text1.txt", "w") as text_any:
@@ -30,8 +16,6 @@ with open("text1.txt", "w") as text_any:
                    " And by opposing end them? To die: to sleep; No more; and by a sleep to say we end The"
                    " heart-ache and the thousand natural shocks That flesh is heir to, 'tis a consummation"
                    " Devoutly to be wish'd. To die, to sleep.")
-
-#
 
 
 # 1. Даний текстовий файл. Необхідно створити новий файл, який потрібно переписати з першого файлу всі слова,
@@ -55,10 +39,10 @@ with open("text7.txt", "r") as text7:
 
 
 with open("text1.txt", "r") as text_any:
-    words = line.split()
-    word_count = 0
-    for word in words:
-        word_count += 1
-print(f"Your text has total {word_count} words")
+    for line in text_any:
+        result = len(line.split(" "))
+    print(result)
+
+
 
 
